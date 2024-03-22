05ca1160b3c41338c22c6ede619277f0577928bc
Ranj0307 <111860308+Ranj0307@users.noreply.github.com>
2024-03-23 00:04:30 +0530

    Add files via upload

--- SaathiCare_React/src/components/NavBar/NavBar.js
+++ SaathiCare_React/src/components/NavBar/NavBar.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { FaSignInAlt, FaCaretDown } from 'react-icons/fa';
 import './NavBar.css';
-import { FaSignInAlt } from 'react-icons/fa';
 
 function NavBar() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleChatbotClick = () => {
+    navigate("/");
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const openDropdown = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
+  const isActivePath = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar-brand">
-        <img src="https://lh3.googleusercontent.com/u/0/drive-viewer/AKGpihbYUgV9yP3x10tHQ3v6MzOoLBoFEHOpEzJJqW-ToQxKk8V5jtO0ngEn_geQloM7I1CLpGcwAh4g42cfrTy_pb-veyge=w1572-h1558" alt="Logo" className="navbar-logo" />
-        <span className="navbar-heading">CareSaathi</span>
+      <div className="navbar-brand" onClick={handleChatbotClick}>
+        <span className={`navbar-heading ${isActivePath('/') ? 'active' : ''}`}>CareSaathi</span>
       </div>
-      <ul className="navbar-nav">
-        <div className="navbar-right">
-          <div className="nav-item">Appointment</div>
+      <div className="navbar-items">
+        <NavLink to="/" className="nav-item" onClick={closeDropdown}>
+          Care Saathi ChatBot
+        </NavLink>
+        <NavLink to="/optimize-lab-report" className="nav-item" onClick={closeDropdown}>
+          Optimize Lab Report
+        </NavLink>
+        <div className="nav-item dropdown" onClick={toggleDropdown} onMouseEnter={openDropdown} onMouseLeave={closeDropdown}>
+          Strategic Healthcare Analytics <FaCaretDown className="dropdown-icon"/>
+          {isDropdownOpen && (
+            <div className="dropdown-content">
+              <NavLink to="/image-analytics" className="dropdown-item" onClick={closeDropdown}>Image Analytics</NavLink>
+              <NavLink to="/extent-prediction" className="dropdown-item" onClick={closeDropdown}>Extend Prediction and Preventive Planning</NavLink>
+            </div>
+          )}
+        </div>
+        <div className="nav-item">
           <button className="login-button">
             <FaSignInAlt className="login-icon" />
             Login
           </button>
         </div>
-      </ul>
+      </div>
     </nav>
   );
 }
