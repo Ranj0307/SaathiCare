2563b1d6d5fb34f902312aeacf6835def98e5b7f
Ranj0307 <111860308+Ranj0307@users.noreply.github.com>
2024-04-29 15:34:51 +0530

    Update api_neo4j.py

--- Saathicare_Backend/api_neo4j.py
+++ Saathicare_Backend/api_neo4j.py
@@ -143,9 +143,9 @@ def process_responses():
 
     if response == '':
         for disease in diseases:
-        with open(f'DiseaseData/{disease_name}.txt', 'w') as file:
-            data_text = file.read()
-            response += data_text
+            with open(f'DiseaseData/{disease_name}.txt', 'w') as file:
+                data_text = file.read()
+                response += data_text
 
     # Return the response as JSON
     return jsonify({'response': response})
